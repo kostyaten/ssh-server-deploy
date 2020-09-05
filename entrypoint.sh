a5f3fd43baa7548671cf09c5eb05c585324c3cd3
Kostya Ten <kostya@yandex.ru>
2020-09-05 18:48:42 +0300

    fix: Checking variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
-ls -la
 
-if ! [ -z $INPUT_BEFORE_SCRIPT ]; then
+if ! [ -z "$INPUT_BEFORE_SCRIPT" ]; then
   CMD="${INPUT_BEFORE_SCRIPT/$'\n'/' && '}"
   sshpass -p $INPUT_PASSWORD ssh -o stricthostkeychecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
 fi
 
-if ! [ -z $INPUT_SCP_SOURCE ]; then
+if ! [ -z "$INPUT_SCP_SOURCE" ]; then
   sshpass -p $INPUT_PASSWORD scp -o stricthostkeychecking=no -P $INPUT_PORT -r $INPUT_SCP_SOURCE $INPUT_USERNAME@$INPUT_HOST:$INPUT_SCP_TARGET
 fi
 
-if ! [ -z $INPUT_AFTER_SCRIPT ]; then
+if ! [ -z "$INPUT_AFTER_SCRIPT" ]; then
   CMD="${INPUT_AFTER_SCRIPT/$'\n'/' && '}"
   sshpass -p $INPUT_PASSWORD ssh -o stricthostkeychecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
 fi
