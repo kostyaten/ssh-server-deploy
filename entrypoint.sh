192f6e6d0e5ba37fef7ac1ed6ce4de511d74aacf
Kostya Ten <kostya@yandex.ru>
2020-11-22 19:29:27 +0300

    pref: Added support for private key
    Signed-off-by: Kostya Ten <kostya@yandex.ru>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ setup_ssh(){
     chmod 600 "$SSH_PATH/known_hosts" "$SSH_PATH/id_rsa"
     eval $(ssh-agent)
     ssh-add "$SSH_PATH/id_rsa"
-#    ssh-keyscan -t rsa $INPUT_HOST >> "$SSH_PATH/known_hosts"
     echo -n ""
 }
 
@@ -20,36 +19,30 @@ if ! [ -z "$INPUT_BEFORE_SCRIPT" ]; then
   CMD="${INPUT_BEFORE_SCRIPT/$'\n'/' && '}"
 
   if ! [ -z "$INPUT_PASSWORD" ]; then
-    sshpass -p $INPUT_PASSWORD ssh -o stricthostkeychecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
+    sshpass -p $INPUT_PASSWORD ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
   fi
 
   if ! [ -z "$INPUT_PRIVATE_KEY" ]; then
-    echo -n "before script start"
     setup_ssh
-    echo -n $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD"
-    echo -n ""
-    ssh -o stricthostkeychecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
-    echo -n "before script end"
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD"
   fi
 
 fi
 
 if ! [ -z "$INPUT_SCP_SOURCE" ]; then
-  sshpass -p $INPUT_PASSWORD scp -o stricthostkeychecking=no -P $INPUT_PORT -r $INPUT_SCP_SOURCE $INPUT_USERNAME@$INPUT_HOST:$INPUT_SCP_TARGET
+  sshpass -p $INPUT_PASSWORD scp -o StrictHostKeyChecking=no -P $INPUT_PORT -r $INPUT_SCP_SOURCE $INPUT_USERNAME@$INPUT_HOST:$INPUT_SCP_TARGET
 fi
 
 if ! [ -z "$INPUT_AFTER_SCRIPT" ]; then
   CMD="${INPUT_AFTER_SCRIPT/$'\n'/' && '}"
 
   if ! [ -z "$INPUT_PASSWORD" ]; then
-    sshpass -p $INPUT_PASSWORD ssh -o stricthostkeychecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
+    sshpass -p $INPUT_PASSWORD ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
   fi
 
   if ! [ -z "$INPUT_PRIVATE_KEY" ]; then
-    echo -n "after script start"
     setup_ssh
-    ssh -o stricthostkeychecking=no -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
-    echo -n "after script end"
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "$CMD";
   fi
 
 fi
